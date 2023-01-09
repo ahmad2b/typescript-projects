88d350225cc5b16f63a23f765b17f0ec0f55ca32
ahmad2b <ahmadshaukat_4@outlook.com>
2023-01-09 15:00:55 +0500

    University Mangagement System

--- proj-usms/src/Announcement.ts
+++ proj-usms/src/Announcement.ts
@@ -3,7 +3,7 @@ import chalk from "chalk";
 let announcements: string[] = [];
 
 announcements.push("Welcome to University Mangagement University");
-announcements.push("\nIts Good to See You Back at University Mangagement University");
+announcements.push("\nIts Good to See You Back at Metaverse University");
 
 export function display(){
     console.log("\n");
